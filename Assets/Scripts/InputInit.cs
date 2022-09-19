a36625822adb26396c447954cc7bed5fc692f13c
FMPO <chrissante3.14@gmail.com>
2022-09-18 21:19:53 -0400

    added a bunch of input management stuff
    remember to fix the bug where the game crashes when you unplug the controllers

--- Assets/Scripts/InputInit.cs
+++ Assets/Scripts/InputInit.cs
@@ -9,21 +9,29 @@ public class InputInit : MonoBehaviour
     public GameObject inputManager;
     private List<GameObject> inputManagers = new List<GameObject>();
 
-    //input count starts at 1 bcs keyboard is always an input
-    private int inputCount = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        //debug check for number of gamepads
         Debug.Log(Gamepad.all.Count);
 
-        //instantiate all of the input managers
-        for (int i = 0; i < inputCount; i++)
+        
+
+        //instantiate all of the gamepad input managers
+        for (int i = 0; i < Gamepad.all.Count; i++)
         {
             inputManagers.Add(Instantiate(inputManager));
             inputManagers[i].GetComponent<InputManage>().inputId = i;
-            Debug.Log($"input Manager {inputManagers[i].GetComponent<InputManage>().inputId} instantiated.");
+            inputManagers[i].GetComponent<InputManage>().gamepadId = Gamepad.all[i];
+            inputManagers[i].GetComponent<InputManage>().isGamepad = true;
+            //InputSystem.AddDevice(Gamepad.all[i]);
+            //InputSystem.AddDeviceUsage(Gamepad.all[i], $"Gamepad {i}");
         }
+
+        //instantiate keyboard as input object
+        inputManagers.Add(Instantiate(inputManager));
+        inputManagers[inputManagers.Count-1].GetComponent<InputManage>().inputId = -1;
     }
 
     // Update is called once per frame
--- Assets/Scripts/InputManage.cs
+++ Assets/Scripts/InputManage.cs
@@ -7,7 +7,8 @@ public class InputManage : MonoBehaviour
 {
     //-------- FIELDS --------
     public int inputId;
-    private bool isGamepad;
+    public bool isGamepad = false;
+    public Gamepad gamepadId;
 
 
 
@@ -22,6 +23,24 @@ public class InputManage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Start of all gamepad input processing
+        if (isGamepad)
+        {
+            if (gamepadId.aButton.IsPressed())
+            {
+                //Also We need to make sure to code the controllers getting removed so the game doesn't crash
+                Debug.Log($":) {inputId}");
+            }
+        }
+        
+        //start of all keyboard input processing
+        else
+        {
+            if (Keyboard.current.spaceKey.isPressed)
+            {
+                Debug.Log($":) {inputId}");
+            }
+        }
         
     }
 }
